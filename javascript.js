41fd0695349d7d512f54fc7b0df3d28a246983ed
leopedreira <leonardo.lcp2005@gmail.com>
2017-11-08 16:14:25 -0200

    Add some ajax tests

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-$('.btn');
+let perguntas = fetch('perguntas.json');
+//perguntas.then( data => data.json());
+setTimeout(() => {
+    perguntas.then( data => data.json())
+             .then( data => data.map( perguntas =>{
+                 console.log(perguntas.questao);
+                 console.log(perguntas.resposta);
+                 $('.alert-heading').text(perguntas.questao);
+                 $('.texto').text(perguntas.resposta);
+             }));
+},2000);
+
+
